a0942c868cd29db0b2f3f974660d03f94383caf6
Haaric <harish.konduru@wipro.com>
2017-11-28 11:22:50 +0530

    removed comments

--- src/main/java/org/rest/dao/DeleteNodesThreads.java
+++ src/main/java/org/rest/dao/DeleteNodesThreads.java
@@ -1,12 +1,7 @@
 package org.rest.dao;
 
-public class DeleteNodesThreads implements Runnable{
-	
-	//private final String deleteQuery = "DELETE FROM NODE WHERE NODE_KEY=?"; 
-	
-	/*@Autowired
-	private JdbcTemplate jdbcTemplate;*/
-	
+public class DeleteNodesThreads implements Runnable {
+
 	private String id;
 
 	public DeleteNodesThreads() {
@@ -18,16 +13,8 @@ public class DeleteNodesThreads implements Runnable{
 		this.id = id;
 	}
 
-	/*public JdbcTemplate getJdbcTemplate() {
-		return jdbcTemplate;
-	}
-
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
-*/
 	@Override
 	public void run() {
-		System.out.println("deleted record with id = "+this.id);
+		System.out.println("deleted record with id = " + this.id);
 	}
 }
